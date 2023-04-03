3cca721ac8160fc85c06745d953658eeaed414b4
Red <red@pop-os.localdomain>
2023-04-03 13:39:30 +0000

    fixed bugs✨

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,27 +2,8 @@ import React from "react";
 
 const LoginPage = () => {
   return (
-    <div class="all">
-    <div class="instagram">
-      <div class="logo">
-        <img src="https://www.sirendesign.com.au/wp-content/themes/siren/library/images/instagram-logo.png" alt="" />
-      </div>
-      <h2>Instagram</h2>
-      <div class="inputer">
-        <input type="text" placeholder="Enter name or email" />
-        <input type="password" placeholder="Enter password" />
-      </div>
-      <div class="buttons">
-        <button>Login</button>
-      </div>
-      <div class="facebook">
-        Continue with Facebook?
-      </div>
-    </div>
-    </div>
-
-
-  )
+    <span>Login page goes here</span>
+    )
 }
 
 export default LoginPage;
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,9 +1,41 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
+  let insta ={
+      fontFamily:"'Billabong', sans-serif",
+      fontSize:"32px",
+      fontWeight:"lighter"
+  }
   return (
     <>
-      <span>For sign up page</span>
+    <div class="all">
+    <div class="instagram">
+      <div class="logo">
+        <img src="https://www.sirendesign.com.au/wp-content/themes/siren/library/images/instagram-logo.png" alt="" />
+      </div>
+      <h2 style={insta}>Instagram</h2>
+      <p style={{alignText: "center"}}>
+      Sign up to see photos and videos <br /> from your friends.
+      <div className="buttons">
+        <button>Log in with facebook</button>
+      </div>
+      OR
+      </p>
+      <div class="inputer">
+        <input type="text" placeholder="Mobile Number or Email" />
+        <input type="text" placeholder="Full Name" />
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Enter password" />
+      </div>
+      <div class="buttons">
+        <button>Sign up</button>
+      </div>
+      <div class="facebook">
+        Have an account? <Link to="/login" style={{textDecoration: "none"}}>Log in</Link>
+      </div>
+    </div>
+    </div>
     </>
   )
 }
